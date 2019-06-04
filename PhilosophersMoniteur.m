9d18770660d6019dffa3259d539254e6d5b1118a
TooPro <thegodlyone95@gmail.com>
2019-06-04 17:37:17 -0400

    update moniteur philosophe

--- PhilosophersMoniteur.m
+++ PhilosophersMoniteur.m
@@ -1,79 +1,65 @@
-global p
+global a
     const nbPhilosophe := 5
-    const round := 10
-    type state = enum(Think, Hungry, Eat)
-    var philosophes[nbPhilosophe]: state
-    var baguettesPicked[nbPhilosophe]: bool
+    var philosophes[nbPhilosophe]: int
 
-    op printPhilosophe(philosopher:int), init()
-    op printValue(s:state) returns r: string[6]
-body p
+    op  init()
+body a
     proc init()
         fa i := 1 to nbPhilosophe ->
-            philosophes[i] := Think
-            baguettesPicked[i]:= false
-            printPhilosophe(i)
+            philosophes[i] := 0
         af
     end
 
-    proc printPhilosophe(i)
-        write("Philosopher", i, printValue(philosophes[i]))
-    end
-
-    proc printValue(s) returns r
-        if s = Think -> r := "Think" fi
-        if s = Hungry -> r := "Hungry" fi
-        if s = Eat -> r := "Eat" fi
-    end
 end
 
 _monitor(pMonitor)
-    import p
-    op pickBaguettes(i:int), dropBaguettes(i:int)
+    import a
+    op pick(i:int), release(i:int)
 _body(pMonitor)
-    _condvar1(eat,p.nbPhilosophe)
+    _condvar1(eat,a.nbPhilosophe)
 
-    _proc(pickBaguettes(i))
-        p.philosophes[i] := Hungry
-        p.printPhilosophe(i)
-        if p.baguettesPicked[i] = false ->
-          if p.baguettesPicked[1 + i mod 5] = false ->
-            if p.philosophes[i] = Hungry ->
-               p.philosophes[i] := Eat
-               p.printPhilosophe(i)
-               baguettesPicked[i] := true
-               baguettesPicked[1 + i mod 5] := true
-               nap(int(random()*1000))
-               _signal(eat[i])
-              fi
-            fi
+    _proc(pick(i))
+        if a.philosophes[i] != 0 ->
+              _wait(eat[i])
           fi
-        if p.philosophes[i] != Eat ->
-            _wait(eat[i])
-        fi
+        a.philosophes[i] := 1;
     _proc_end
 
-    _proc(dropBaguettes(i))
-        p.philosophes[i] := Think
-        p.printPhilosophe(i)
-        baguettesPicked[i] := false
-        baguettesPicked[1 + i mod 5] := false
-        _signal(eat[i])
+    _proc(release(i))
+       if a.philosophes[i] != 0 ->
+          a.philosophes[i] := 0
+          _signal(eat[i])
+          fi
     _proc_end
 _monitor_end
 
 
 resource main()
-    import p
+    import a
     import pMonitor
-
-    p.init()
-
-    process ps(i:=1 to p.nbPhilosophe)
-        fa k := 0 to p.round ->
-            pMonitor.pickBaguettes(i)
-            pMonitor.dropBaguettes(i)
-            k++
-        af
+    a.init()
+    process ps(i:=1 to a.nbPhilosophe-1)
+        do true ->
+        pMonitor.pick(i)
+        pMonitor.pick(1+i mod 5)
+        write("Philosopher", i, "begin to eat")
+        nap(int(random())*100)
+        write("Philosopher", i, "finish to eat")
+        pMonitor.release(i)
+        pMonitor.release(1+i mod 5)
+        write("Philosopher", i, "Think")
+        od
     end
+  process ps5
+        do true ->
+          pMonitor.pick(1)
+          pMonitor.pick(5)
+          write("Philosopher", 5, "begin to eat")
+          nap(int(random())*100)
+          write("Philosopher", 5, "finish to eat")
+          pMonitor.release(1)
+          pMonitor.release(5)
+          write("Philosopher", 5, "Think")
+          od
+  end
 end
